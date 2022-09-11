b0fb829d4c2077223f30b2724cdfc5802b47bed3
matyjb <2fishx@gmail.com>
2022-09-11 18:44:46 +0200

    use _dio

--- pixoo_twitch_emotes_displayer/lib/services/tEmotes_api.dart
+++ pixoo_twitch_emotes_displayer/lib/services/tEmotes_api.dart
@@ -1,12 +1,11 @@
 import 'package:dio/dio.dart';
-
 import '../models/emote/emote.dart';
 
 class TEmotesAPI {
   static final Dio _dio = Dio();
 
   static Future<List<Emote>> getEmotesForChannel(String channelName) async {
-    var response = await Dio()
+    var response = await _dio
         .get('https://emotes.adamcy.pl/v1/channel/$channelName/emotes/all');
     return (response.data as List<dynamic>)
         .map((e) => Emote.fromJson(e))
@@ -15,14 +14,14 @@ class TEmotesAPI {
 
   static Future<List<Emote>> getEmotesGlobal() async {
     var response =
-        await Dio().get('https://emotes.adamcy.pl/v1/global/emotes/all');
+        await _dio.get('https://emotes.adamcy.pl/v1/global/emotes/all');
     return (response.data as List<dynamic>)
         .map((e) => Emote.fromJson(e))
         .toList();
   }
 
   static Future<List<Emote>> getChannelIdentifiers(String channelName) async {
-    var response = await Dio()
+    var response = await _dio
         .get('https://emotes.adamcy.pl/v1/channel/$channelName/id');
     return (response.data as List<dynamic>)
         .map((e) => Emote.fromJson(e))
